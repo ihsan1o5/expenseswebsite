91f8a4d2abc525ac3cf43e44364e9c58eb7f585c
ihsan1o5 <ihsanullahi501@gmail.com>
2020-08-17 10:36:13 +0500

    this is the first commit

---
+++ expenses/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from . import views
+
+# app_name = "expenses"
+
+urlpatterns = [
+	path('', views.index, name="expenses"),
+	path('add-expense', views.add_expense, name="add-expenses"),
+]
